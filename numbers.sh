76fca0650b506ea1b77ebdba78bcd4286f286348
Emily Nguyen <eminguyen@chapman.edu>
2020-11-18 11:20:18 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+# numbers.sh
+# Emily Nguyen
+
+echo "Enter a positive number: "
+read number
+
+i=1
+while [ $i -le $number ]
+do
+	if [ $((i % 2)) -eq 0 ]
+	then
+		echo "$i even"
+	else
+		echo "$i odd"
+	fi
+	i=$(($i + 1))
+done
